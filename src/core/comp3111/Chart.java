6d44239bd0a3c2169c4c0b8fc140df3cd44a55aa
sheldonchiu <32926567+sheldonchiu@users.noreply.github.com>
2018-04-22 19:48:12 +0800

    Finished animated chart

--- src/core/comp3111/Chart.java
+++ src/core/comp3111/Chart.java
@@ -1,8 +1,6 @@
 package core.comp3111;
 
-import javafx.scene.chart.XYChart;
-import javafx.scene.chart.XYChart.Series;
-
 public interface Chart {
-	public XYChart.Series<Number, Number> getSeries();
+	public void setTitle(String m_title);
+	public String getTitle();
 }
