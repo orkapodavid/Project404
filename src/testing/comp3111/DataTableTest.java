8bafa90bab8d9ab57976f38ab477e572e538f863
WRY <gundamexia1997@gmail.com>
2018-05-02 22:53:39 +0800

    Added documentaion for test cases

--- src/testing/comp3111/DataTableTest.java
+++ src/testing/comp3111/DataTableTest.java
@@ -17,6 +17,11 @@ import core.comp3111.DataType;
  */
 class DataTableTest {
 	
+	/**
+	 * Test case for default constructor of the DataTable class.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableConstructor() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -24,8 +29,14 @@ class DataTableTest {
 		assert(testDataTable.getNumRow() == 0);
 	}
 	
+	/**
+	 * Test case for the addCol method in the DataTable class.
+	 * This test case covers the case where DataColumns are inserted into a DataTable successfully.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
-	void testDataTableAddColNormal() throws DataTableException {
+	void testDataTableAddCol() throws DataTableException {
 		DataTable testDataTable = new DataTable();
 		DataColumn testDataColumn1 = new DataColumn();
 		DataColumn testDataColumn2 = new DataColumn();
@@ -40,16 +51,12 @@ class DataTableTest {
 		assert(testDataTable.getNumCol() == 2);
 	}
 	
-	@Test
-	void testDataTableAddColEmptyTable() throws DataTableException {
-		DataTable testDataTable = new DataTable();
-		DataColumn testDataColumn = new DataColumn();
-		String colName = "Test";
-		
-		testDataTable.addCol(colName, testDataColumn);
-		assert(testDataTable.getNumCol() == 1);
-	}
-	
+	/**
+	 * Test case for the addCol method in the DataTable class.
+	 * This test case covers the case where a DataColumn is inserted into a DataTable unsuccessfully due to having an identical name with an existing DataColumn in the DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableAddColSameNameException() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -60,6 +67,12 @@ class DataTableTest {
 		assertThrows(DataTableException.class, () -> testDataTable.addCol(colName, testDataColumn));
 	}
 	
+	/**
+	 * Test case for the addCol method in the DataTable class.
+	 * This test case covers the case where a DataColumn is inserted into a DataTable unsuccessfully due to having a different row count when compared to existing DataColumns in the DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableAddColDiffRowCountException() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -75,6 +88,12 @@ class DataTableTest {
 		assertThrows(DataTableException.class, () -> testDataTable.addCol(colName2, testDataColumn2));
 	}
 	
+	/**
+	 * Test case for the removeCol method in the DataTable class.
+	 * This test case covers the case where a DataColumn is successfully removed from a DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableRemoveCol() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -88,6 +107,12 @@ class DataTableTest {
 		assert(testDataTable.getNumRow() == 0);
 	}
 	
+	/**
+	 * Test case for the removeCol method in the DataTable class.
+	 * This test case covers the case where a DataColumn is unsuccessfully removed from a DataTable as it does not exists in the DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableRemoveColNotExistException() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -96,6 +121,12 @@ class DataTableTest {
 		assertThrows(DataTableException.class, () -> testDataTable.removeCol(colName1));
 	}
 	
+	/**
+	 * Test case for the getCol method in the DataTable class.
+	 * This test case covers the case where a DataColumn is successfully retrieved from a DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetCol() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -108,6 +139,12 @@ class DataTableTest {
 		assert(testDataTable.getCol(colName1).getData().equals(testCol1));
 	}
 	
+	/**
+	 * Test case for the getCol method in the DataTable class.
+	 * This test case covers the case where a DataColumn is not retrieved from a DataTable as it does not exist.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetColNotFound() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -120,6 +157,12 @@ class DataTableTest {
 		assert(testDataTable.getCol(colName2) == null);
 	}
 	
+	/**
+	 * Test case for the getAllNumColName method in the DataTable class.
+	 * This test case covers the case where the names of all numeric columns in a DataTable is retrieved.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetAllNumColName() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -136,6 +179,12 @@ class DataTableTest {
 		assert(testDataTable.getAllNumColName().length == 1);
 	}
 	
+	/**
+	 * Test case for the getAllNumColName method in the DataTable class.
+	 * This test case covers the case where no names of any numeric columns in a DataTable is retrieved due to an empty DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetAllNumColNameEmptyTable() {
 		DataTable testDataTable = new DataTable();
@@ -143,6 +192,12 @@ class DataTableTest {
 		assert(testDataTable.getAllNumColName() == null);
 	}
 	
+	/**
+	 * Test case for the getAllTextColName method in the DataTable class.
+	 * This test case covers the case where the names of all string columns in a DataTable is retrieved.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetAllTextColName() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -159,6 +214,12 @@ class DataTableTest {
 		assert(testDataTable.getAllTextColName().length == 1);
 	}
 	
+	/**
+	 * Test case for the getAllTextColName method in the DataTable class.
+	 * This test case covers the case where no names of any string columns in a DataTable is retrieved due to an empty DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetAllTextColNameEmptyTable() {
 		DataTable testDataTable = new DataTable();
@@ -166,6 +227,12 @@ class DataTableTest {
 		assert(testDataTable.getAllTextColName() == null);
 	}
 	
+	/**
+	 * Test case for the getNumOfNumCol method in the DataTable class.
+	 * This test case covers the case where the number of numeric columns in a DataTable is retrieved.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetNumOfNumCol() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -182,6 +249,12 @@ class DataTableTest {
 		assert(testDataTable.getNumOfNumCol() == 1);
 	}
 	
+	/**
+	 * Test case for the getNumOfNumCol method in the DataTable class.
+	 * This test case covers the case where the number of numeric columns in an empty DataTable is retrieved.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetNumOfNumColEmptyTable() {
 		DataTable testDataTable = new DataTable();
@@ -189,6 +262,12 @@ class DataTableTest {
 		assert(testDataTable.getNumOfNumCol() == 0);
 	}
 	
+	/**
+	 * Test case for the getNumOfTextCol method in the DataTable class.
+	 * This test case covers the case where the number of string columns in a DataTable is retrieved.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetNumOfTextCol() throws DataTableException {
 		DataTable testDataTable = new DataTable();
@@ -205,6 +284,12 @@ class DataTableTest {
 		assert(testDataTable.getNumOfTextCol() == 1);
 	}
 	
+	/**
+	 * Test case for the getNumOfTextCol method in the DataTable class.
+	 * This test case covers the case where the number of string columns in an empty DataTable is retrieved.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetNumOfTextColEmptyTable() {
 		DataTable testDataTable = new DataTable();
@@ -212,8 +297,14 @@ class DataTableTest {
 		assert(testDataTable.getNumOfTextCol() == 0);
 	}
 	
+	/**
+	 * Test case for the getAllColName method in the DataTable class.
+	 * This test case covers the case where the names of all columns in a DataTable is retrieved.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
-	void testDAtaTableGetAllColName() throws DataTableException {
+	void testdataTableGetAllColName() throws DataTableException {
 		DataTable testDataTable = new DataTable();
 		DataColumn testDataColumn1 = new DataColumn();
 		DataColumn testDataColumn2 = new DataColumn();
@@ -228,6 +319,12 @@ class DataTableTest {
 		assert(testDataTable.getAllColName().length == 2);
 	}
 	
+	/**
+	 * Test case for the getAllColName method in the DataTable class.
+	 * This test case covers the case where no names of any columns in a DataTable is retrieved due to an empty DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testDataTableGetAllColNameEmptyTable() {
 		DataTable testDataTable = new DataTable();
@@ -235,6 +332,12 @@ class DataTableTest {
 		assert(testDataTable.getAllColName() == null);
 	}
 	
+	/**
+	 * Test case for the print method in the DataTable class.
+	 * This test case simply prints out a DataTable.
+	 * 
+	 * @author kwaleung
+	 */
 	@Test
 	void testPrint() throws DataTableException {
 		DataTable testDataTable = new DataTable();
